8afb588871f050e0568ffd00cebc364de394c961
Jason Carlson <jaekar99@gmail.com>
2018-01-16 10:47:31 -0800

    Update ATTintRainbow.ino
    Cleaned up bootloader and fuse instructions

--- ATTintRainbow.ino
+++ ATTintRainbow.ino
@@ -1,11 +1,20 @@
+
+// If flashing on Sparkfun ATTiny flasher remember to burn bootloader
+// This will set the fuses at the correct speed- 8Mhz internal
+// Then you can flash the Neopix sketch
+// If you are getting all white or odd patterns - bootloader is not burned..
+// Skip this if you are using the Kickstarter ATTIny85
+
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
 
-#define PIN 4     // This is pin number P4 on the Digispark
-#define NEONUM 12  // The number of Neopixels
+#define PIN 4
+#define NEONUM 12
 
+// PIN Def - Pin number that the data signal will be on the chip to the pixels
+// NEONUM Def - The number of pixels on the strip
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
 // Parameter 3 = pixel type flags, add together as needed:
@@ -13,7 +22,8 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NEONUM, PIN, NEO_GRB + NEO_KHZ800);
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NEONUM, PIN, NEO_RGB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
@@ -23,17 +33,14 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(NEONUM, PIN, NEO_GRB + NEO_KHZ800);
 void setup() {
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
-  strip.setBrightness(64); //Setting the brightness
-
-  }
+  strip.setBrightness(200);
+}
 
 void loop() {
 
   rainbowCycle(20);
 
 }
-
-
 
 
 
@@ -52,18 +59,18 @@ void rainbowCycle(uint8_t wait) {
 
 
 
+
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
   if(WheelPos < 85) {
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else if(WheelPos < 170) {
-    WheelPos -= 85;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
   }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
-
